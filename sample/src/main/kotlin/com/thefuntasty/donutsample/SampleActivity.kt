950afaa2011d081741e04c91cb405f80b4d5502a
matsem <matej.semancik@gmail.com>
2019-10-15 17:21:38 +0200

    Add sample activity with prepopulated view

--- sample/src/main/kotlin/com/thefuntasty/donutsample/SampleActivity.kt
+++ sample/src/main/kotlin/com/thefuntasty/donutsample/SampleActivity.kt
@@ -2,11 +2,27 @@ package com.thefuntasty.donutsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.thefuntasty.donut.DonutProgressEntry
+import kotlinx.android.synthetic.main.activity_sample.*
 
 class SampleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample)
+
+        donut_view.apply {
+            cap = 5f
+            submitEntries(
+                listOf(
+                    DonutProgressEntry("broccoli", 1.5f, getColorCompat(R.color.dark_green)),
+                    DonutProgressEntry("avocado", 1.2f, getColorCompat(R.color.light_green)),
+                    DonutProgressEntry("eggplant", 0.5f, getColorCompat(R.color.light_violet))
+                )
+            )
+        }
     }
+
+    private fun AppCompatActivity.getColorCompat(id: Int) = ContextCompat.getColor(this, id)
 }
