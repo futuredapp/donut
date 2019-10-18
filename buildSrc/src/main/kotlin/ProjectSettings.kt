20f241c435b1277bbab47fd54d675702165b9d2f
Lukas -skywall- Sztefek <lukas.sztefek@thefuntasty.com>
2019-10-18 23:01:18 +0200

    Add support for publish to MavenCentral

--- buildSrc/src/main/kotlin/ProjectSettings.kt
+++ buildSrc/src/main/kotlin/ProjectSettings.kt
@@ -2,5 +2,28 @@ object ProjectSettings {
     const val applicationId = "com.thefuntasty.donut"
     const val targetSdk = 29
     const val minSdk = 21
-    const val group = "com.github.thefuntasty"
+    const val group = "com.thefuntasty.donut"
+
+    /**
+     * this version will be used only for local builds, jitpack will automatically provide TAG version
+     */
+    val version = System.getenv("BITRISE_GIT_TAG") ?: "0.0.1-TEST"
+
+    object Donut {
+        const val artifact = "donut"
+        const val libraryDescription =
+            "Donut is doughnut-like graph view capable of showing multiple datasets with assignable colors"
+    }
+
+    object Publish {
+        const val bintrayRepo = "donut"
+        const val siteUrl = "https://github.com/thefuntasty/donut"
+        const val gitUrl = "https://github.com/thefuntasty/donut.git"
+        const val developerId = "TheFuntasty"
+        const val developerName = "TheFuntasty"
+        const val developerEmail = "ops@thefuntasty.com"
+        const val licenseName = "MIT Licence"
+        const val licenseUrl = "https://github.com/thefuntasty/donut/blob/master/LICENCE"
+        val allLicenses = listOf("MIT")
+    }
 }
