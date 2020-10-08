2e7a353c83d7279259c6b988ae1d324f47cdf3ec
matejsemancik <matej.semancik@gmail.com>
2020-10-08 22:16:35 +0200

    Fix wildcard imports

--- library/src/main/kotlin/app/futured/donut/DonutProgressLine.kt
+++ library/src/main/kotlin/app/futured/donut/DonutProgressLine.kt
@@ -1,6 +1,12 @@
 package app.futured.donut
 
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.ComposePathEffect
+import android.graphics.CornerPathEffect
+import android.graphics.DashPathEffect
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.PathMeasure
 import kotlin.math.ceil
 
 internal class DonutProgressLine(
