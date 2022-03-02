c6cd540ab37e00aa25ea40425d7916fd50a28de7
matejsemancik <matej.semancik@gmail.com>
2022-03-02 16:20:34 +0100

    Bump dokka version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,7 +10,7 @@ object Versions {
     const val ktlint = "8.2.0"
     const val ktlintExtension = "0.34.2"
     const val mavenPublish = "0.14.2"
-    const val dokka = "1.4.20"
+    const val dokka = "1.6.10"
 
     // androidx
     const val appCompat = "1.3.1"
