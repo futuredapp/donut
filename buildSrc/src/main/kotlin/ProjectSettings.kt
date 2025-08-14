083f9cb4332428d69ce367e4ad50bd9313d1adb7
Jacob Derynk <jacob.derynk@futured.app>
2025-08-14 15:29:15 +0200

    Feat: Target SDK update to 36
    Target SDK updated from 33 to 36 to ensure compatibility with newer Android versions and features.

--- buildSrc/src/main/kotlin/ProjectSettings.kt
+++ buildSrc/src/main/kotlin/ProjectSettings.kt
@@ -1,6 +1,6 @@
 object ProjectSettings {
     const val applicationId = "app.futured.donut"
-    const val targetSdk = 33
+    const val targetSdk = 36
     const val minSdkLibrary = 19
     const val minSdkLibraryCompose = 21
     const val minSdkSample = 21
